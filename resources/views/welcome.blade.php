82d97a87827aa3e25e4e79b57f107a40f839f979
Leart Bajrami <73801402+Leart-C@users.noreply.github.com>
2026-01-01 23:37:03 +0100

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel CRUD</title>
+</head>
+<body>
+    <h1>Welcome to Laravel CRUD</h1>
+    <p>Click the button below to view the list of laravel</p>
+
+    <a href="" class="btn">Find Laravel!</a>
+</body>
+</html>
