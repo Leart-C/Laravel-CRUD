b7cc71171589b806a5b97dacd86ad8f5e865d3c4
Leart Bajrami <73801402+Leart-C@users.noreply.github.com>
2026-01-02 12:24:58 +0100

    Created a shortcut where table find the id and manipulates

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ Route::get('/laravel', [LaravelController::class, 'index'])->name('laravel.index
 
 Route::get('/laravel/create', [LaravelController::class, 'create'])->name('laravel.create');
 
-Route::get('/laravel/{id}', [LaravelController::class, 'show'])->name('laravel.show');
+Route::get('/laravel/{laravel}', [LaravelController::class, 'show'])->name('laravel.show');
 
 Route::post('/laravel', [LaravelController::class, 'store'])->name('laravel.store');
 
-Route::delete('/laravel/{id}', [LaravelController::class, 'destroy'])->name('laravel.destroy');
+Route::delete('/laravel/{laravel}', [LaravelController::class, 'destroy'])->name('laravel.destroy');
